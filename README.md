8a03350ef650a3d815bfd5cb6b08a512d877f25e
Turner Jacobs <tsjacobs3@gmail.com>
2019-10-17 14:05:10 +0400

    Added functional reset button, semi-functional resize button

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # etch-a-sketch
 
 This project aims to recreate the classic children's toy, the Etch-a-Sketch, in a browser-based format.
+
+TO DO:
+1. Make grid squares proportional in style.css
+2. Add "Resize" button functionality
+
--- script.js
+++ script.js
@@ -1,21 +1,81 @@
-//Create grid squares
+//global variables
 const gridContainer = document.querySelector('#gridContainer');
+const gridItems = document.createElement('div');
+const squares = document.querySelectorAll('.gridItem');
+
+//Create grid squares
+//const gridContainer = document.querySelector('#gridContainer');
 for (i = 0; i < 256; i++) {
   const gridItems = document.createElement('div');
   gridContainer.appendChild(gridItems);
   gridItems.classList.add('gridItem');
-}
+  assignGridId();
+};
 
 //Assign gridItem IDs
-const squares = document.querySelectorAll('.gridItem');
-for (let i = 0; i < squares.length; i++) {
-  squares[i].id = 'square' + i;
-}
-  
-//Hover event listener
-document.querySelectorAll('.gridItem').forEach((div) => {
-  div.addEventListener('mouseenter', e => {
-    div.classList.add('hoverEffect');
-    console.log(div.id);
+function assignGridId() {
+  const squares = document.querySelectorAll('.gridItem');
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].id = i + 1;
+  };
+}  
+
+//Reset Function
+function resetGrid() {
+  document.querySelectorAll('.gridItem').forEach((div) => {
+    div.classList.remove('hoverEffect');
+  });
+};
+
+//Reset Button
+document.querySelectorAll('#reset').forEach((button) => {
+  button.addEventListener('click', e => {
+    resetGrid();
   });
 });
+
+//Resize Function
+function resizeGrid() {
+  let userInput = window.prompt('How many squares per side?', '???');
+  resetGrid();
+  if (userInput < 256) {
+    console.log('less than 256');
+    for (i = 256; i > (256 - userInput); i--) {
+      //remove 'i' child nodes
+    }
+  } else {
+    console.log(userInput);
+    for (i = 256; i < userInput; i++) {
+      const gridItems = document.createElement('div');
+      gridContainer.appendChild(gridItems);
+      gridItems.classList.add('gridItem');
+      assignGridId();
+    }
+  }
+  mouseOver();
+}
+
+//Resize Button
+document.querySelectorAll('#resize').forEach((button) => {
+  button.addEventListener('click', e => {
+   resizeGrid();
+  });
+});
+
+//Mouseover event listener
+document.querySelectorAll('.gridItem').forEach((div) => {
+  div.addEventListener('mouseover', e => {
+    div.classList.add('hoverEffect');
+    //console.log(div.id);
+  });
+});
+
+//Mouseover function
+function mouseOver() {
+  document.querySelectorAll('.gridItem').forEach((div) => {
+    div.addEventListener('mouseover', e => {
+      div.classList.add('hoverEffect');
+      //console.log(div.id);
+    });
+  });
+}
